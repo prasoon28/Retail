0d3bad906f12605120a5962062e14d01bb89c377
CGI <CGI>
2019-05-10 10:08:01 +0530

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowData()
+        {
+            Student s1 = new Student { Studid = 23, Name = "Parag", Sem = 1 };
+            List<Student> l1 = new List<Student>();
+            l1.Add(s1);
+            return View(l1);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class Student
+    {
+        int studid;
+        string name;
+        int sem;
+
+        public int Studid
+        {
+            get
+            {
+                return studid;
+            }
+
+            set
+            {
+                studid = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Sem
+        {
+            get
+            {
+                return sem;
+            }
+
+            set
+            {
+                sem = value;
+            }
+        }
+    }
+}
